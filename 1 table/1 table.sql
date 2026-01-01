98a14b1d9178a8669bfd581f2dd82761ed54413e
yeruchemrapaport <yeruchemrapaport@gmail.com>
2026-01-01 15:02:15 +0200

    Add planet to the body table

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -2,7 +2,8 @@ USE humanbodydb
 GO
 drop table if exists body
 create table dbo.body(
-id int not null identity primary key, 
+id int not null identity primary key,
+planet varchar (50) not null,
 bodypart varchar(100) not null, 
 num int null
 )
