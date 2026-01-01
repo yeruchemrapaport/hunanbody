b017e82c1911244748d9bc1c90a8db3b79fd66cf
yeruchemrapaport <yeruchemrapaport@gmail.com>
2026-01-01 14:55:29 +0200

    1 commit for the trial branch

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -1,6 +1,10 @@
-USE humanbodydb
+USE humanbodydb 
 GO
-create table dbo.body(id int not null identity primary key, 
-bodypart varchar(100) not null, 
-num int not null
+drop table if exists body
+go
+create table dbo.body(
+   id int not null identity primary key,
+   creaturetype varchar(25) not null,
+   bodypart varchar(100) not null, 
+   num int not null
 )
